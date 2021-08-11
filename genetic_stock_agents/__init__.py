14ac5ce51d0f567eba3922b62e90064f2334c21b
Binod Rimal <37233232+binodrimal@users.noreply.github.com>
2021-08-11 13:44:20 -0400

    Update __init__.py

--- genetic_stock_agents/__init__.py
+++ genetic_stock_agents/__init__.py
@@ -1,1 +1,9 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+from datetime import date
+!pip install yfinance
+import yfinance as yf
+from scipy import stats
